d4c72ddb11521ca97e3c41c8d5b7fd405bfb143a
Derek Brown <derekbrown121@gmail.com>
2013-11-06 13:56:18 -0500

    Version to 0.1.3-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject theladders/lein-uberjar-deploy "0.1.1"
+(defproject theladders/lein-uberjar-deploy "0.1.3-SNAPSHOT"
   :description "Create and Deploy uberjar."
   :license {:name "MIT"
             :url "http://www.opensource.org/licenses/mit-license.php"}
@@ -7,5 +7,10 @@
                  [org.clojure/data.xml "0.0.7"]
                  [org.clojure/data.zip "0.1.1"]
                ]
+      :deploy-repositories [
+                                                      ["snapshots" {:id "nexus" :url "http://mercurial:8081/nexus/content/repositories/snapshots"}]
+                                                      ["releases"  {:sign-releases false :id "nexus" :url "http://mercurial:8081/nexus/content/repositories/releases"}]
+                                                     ] 
+
 )
 
