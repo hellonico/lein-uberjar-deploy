c869867acabcac9df218041d930a94b3997ea37b
Sean T Allen <sean@monkeysnatchbanana.com>
2013-12-09 09:40:08 -0500

    Clean up formatting.

--- project.clj
+++ project.clj
@@ -3,9 +3,6 @@
   :license {:name "MIT"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :eval-in-leiningen true
-  :dependencies[
-                 [org.clojure/data.xml "0.0.7"]
-                 [org.clojure/data.zip "0.1.1"]
-               ]
-)
+  :dependencies[[org.clojure/data.xml "0.0.7"]
+                [org.clojure/data.zip "0.1.1"]])
 
